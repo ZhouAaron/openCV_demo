0154fe63898b9a22bbc4a82fee69b0cf9e877331
ZhouAaron <zj971021@163.com>
2021-05-14 19:13:26 +0800

    fixed thresholding

--- image-Thresholding/cv2_thresholding.py
+++ image-Thresholding/cv2_thresholding.py
@@ -4,4 +4,26 @@ import matplotlib.pyplot as plt
 img = cv2.imread("./image/gradient.jpg",0)
 ret,th = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 cv2.imshow('thresh',th)
+
+#应用5种不同的阈值方法
+ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+ret, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+ret, th4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+ret, th5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
+
+titles = ['Original', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, th1, th2, th3, th4, th5]
+for i in  range(6):
+    plt.subplot(2, 3, i + 1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i],fontsize = 8)
+    plt.xticks([]),plt.yticks([])# 隐藏坐标轴
+
+plt.show()
+
+
+
+
+
 cv2.waitKey(0)
